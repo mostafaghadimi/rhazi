c6498d8de44ef66ca5b9c0d5b7360adfcaa94da0
Mostafa <mostafa.ghadimi@yahoo.com>
2019-08-25 16:04:59 +0430

    names added to counter

--- code.py
+++ code.py
@@ -1,25 +1,40 @@
 import pandas as pd
 
 
+def question_category_count(categories, counts):
+    length = len(categories)
+    q_info = dict()
+
+    for i in range(length):
+        q_info[categories[i]] = counts[i]
+
+    return q_info
+
 pd.set_option('display.expand_frame_repr', False)
 
-dataframe = pd.read_excel(r'./files/consultationQuestionsـv2.xlsx')
 
-df = pd.DataFrame(dataframe)
+questions_file = pd.read_excel(r'./files/consultationQuestionsـv2.xlsx')
 
-medical_section = df["medicalSection_id"].fillna(0.0).astype(int)
+questions_data_frame = pd.DataFrame(questions_file)
+
+categories_file = pd.read_excel(r'./files/medicalSections.xlsx')
+categories_data_frame = pd.DataFrame(categories_file)["medicalSections"]
+
+categories = []
+for i in categories_data_frame:
+    categories.append(i)
+
+medical_section = questions_data_frame["medicalSection_id"].fillna(0.0).astype(int)
 count_sections = [0 for _ in range(51)]
-
-
 
 for id in medical_section:
     if id:
         count_sections[id] += 1
+count_sections = count_sections[1:] + [0]
 
-print(count_sections)
+q_info = question_category_count(categories, count_sections)
+print(q_info)
 
 
-# print(dataframe)
 
-# for id in dataframe.iterrows():
-    # print(id.split(" "))
+
