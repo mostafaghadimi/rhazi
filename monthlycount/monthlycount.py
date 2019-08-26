29cc83ac612c7389b29975ad116c031fde98f3fa
Mostafa <mostafa.ghadimi@yahoo.com>
2019-08-26 19:38:57 +0430

    finalize monthlycount

--- monthlycount/monthlycount.py
+++ monthlycount/monthlycount.py
@@ -3,6 +3,7 @@ import numpy as np
 
 excel_file = pd.read_excel(r'../files/consultationQuestionsـv2.xlsx')
 questions_data_frame = pd.DataFrame(excel_file, columns=["medicalSection_id", "created"])
+questions_data_frame = questions_data_frame.dropna()
 
 time_intervals = pd.period_range(start="2014-04", end="2019-08", freq="M")
 medical_id = np.array(range(1,52))
@@ -10,7 +11,12 @@ medical_id = np.array(range(1,52))
 monthly_data_frame = pd.DataFrame({'count': 0}, index= time_intervals, columns=medical_id)
 monthly_data_frame = monthly_data_frame.fillna(0).astype(int)
 
-for index, row in questions_data_frame.head().iterrows():
-    monthly_data_frame.loc[row['created'][:7], row["medicalSection_id"]] += 1
+for index, row in questions_data_frame.iterrows():
 
-print(monthly_data_frame)
+    if row['medicalSection_id']:
+        monthly_data_frame.loc[row['created'][:7], row["medicalSection_id"]] += 1
+
+np.savetxt(r'./output.txt', monthly_data_frame, fmt='%s')
+
+# Check the program works correcly
+# print(monthly_data_frame.sum(axis=0, skipna=True))
