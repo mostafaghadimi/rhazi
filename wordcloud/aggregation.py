1f7a7349da28e5fe457f66c1677cd52548d770d0
Mostafa <mostafa.ghadimi@yahoo.com>
2019-09-03 19:57:39 +0430

    Allaaaah

--- wordcloud/aggregation.py
+++ wordcloud/aggregation.py
@@ -1,6 +1,15 @@
+from hazm import Normalizer, word_tokenize
 import pandas as pd
 import numpy as np
 
+pd.options.display.max_rows
+pd.set_option('display.max_colwidth', -1)
+
+# Persian Stops Word List
+with open('./persian-stops.txt', encoding='utf-8') as stop_file:
+    stop_words = stop_file.read().splitlines()
+
+# Reading input
 excel_file = pd.read_excel(r'../files/consultationQuestionsـv2.xlsx')
 
 question_data_frame = pd.DataFrame(excel_file)
@@ -13,5 +22,21 @@ question_join = question_group['question'].apply(' '.join)[1:]
 question_sets = pd.DataFrame(question_join)
 
 for index, question in question_sets.iterrows():
-    with open(r'./input/{}.txt'.format(int(index)), 'w', encoding='utf-8') as file:
-        np.savetxt(file, question, fmt="%s")
+    normalizer = Normalizer()
+    normal_text = normalizer.normalize(str(question))
+    word_list = word_tokenize(normal_text)
+    new_word_list = []
+    for i in range(len(word_list)):
+        # print(word_list[i])
+        inserted = word_list[i]
+        for stop in stop_words:
+            if word_list[i] == stop:
+                inserted = ""
+        # print(inserted)
+        new_word_list.append(inserted)
+
+    text = " ".join(map(str, new_word_list))
+    # print(text)
+    with open(r'./output/{}.txt'.format(int(index)), 'w', encoding='utf-8') as file:
+        # np.savetxt(file, text)
+        file.write(text)
